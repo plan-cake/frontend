3041b284f8979ac046b0878b478c45893f274f15
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-07-19 14:23:13 -0400

    Restructure Event Info Drawer

--- app/ui/components/event-info-drawer.tsx
+++ app/ui/components/event-info-drawer.tsx
@@ -13,7 +13,7 @@ export default function EventInfoDrawer({
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className="rounded-full hover:bg-gray-100 md:hidden dark:border-gray-400 dark:hover:bg-gray-700">
+        <button className="rounded-full hover:bg-gray-100 focus:outline-none md:hidden dark:border-gray-400 dark:hover:bg-gray-700">
           <InfoCircledIcon width={20} height={20} />
         </button>
       </Dialog.Trigger>
@@ -21,7 +21,7 @@ export default function EventInfoDrawer({
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 z-40 bg-black/50" />
         <Dialog.Content
-          className="fixed right-0 bottom-0 left-0 z-50 flex h-[500px] w-full animate-slideUp flex-col rounded-t-3xl bg-white p-8 data-[state=closed]:animate-slideDown dark:bg-[#343249]"
+          className="fixed right-0 bottom-0 left-0 z-50 flex h-[500px] w-full animate-slideUp flex-col rounded-t-3xl bg-white p-8 focus:outline-none data-[state=closed]:animate-slideDown dark:bg-[#343249]"
           aria-label="Event Info"
         >
           <EventInfo eventRange={eventRange} />
@@ -34,30 +34,39 @@ export default function EventInfoDrawer({
 export function EventInfo({ eventRange }: { eventRange: EventRange }) {
   return (
     <section>
-      <h2 className="mb-2 text-lg font-semibold">Event Details</h2>
-      <div>
-        <span className="font-medium text-blue dark:text-red">
-          Possible Dates:
-        </span>
-        {eventRange.type === "specific" ? (
-          <span>
-            {eventRange.dateRange.from?.toLocaleDateString()} –{" "}
-            {eventRange.dateRange.to?.toLocaleDateString()}
-          </span>
-        ) : (
-          <span>
-            {Object.entries(eventRange.weekdays)
-              .filter(([_, isSelected]) => isSelected)
-              .map(([day]) => day)
-              .join(", ")}
-          </span>
-        )}
+      <div className="sticky top-0 mb-4 flex items-center justify-between">
+        <div className="text-lg font-semibold">Event Details</div>
       </div>
-      <div>
-        <span className="font-medium text-blue dark:text-red">
-          Possible Times:
-        </span>
-        <div>
+
+      <div className="space-y-4 overflow-y-auto">
+        {eventRange.type === "specific" ? (
+          <>
+            <InfoRow label="Possible Dates">
+              {eventRange.dateRange.from?.toLocaleDateString(undefined, {
+                weekday: "short",
+                month: "short",
+                day: "numeric",
+              })}{" "}
+              –{" "}
+              {eventRange.dateRange.to?.toLocaleDateString(undefined, {
+                weekday: "short",
+                month: "short",
+                day: "numeric",
+              })}
+            </InfoRow>
+          </>
+        ) : (
+          <>
+            <InfoRow label="Days of the Week">
+              {Object.entries(eventRange.weekdays)
+                .filter(([_, val]) => val === 1)
+                .map(([day]) => day)
+                .join(", ")}
+            </InfoRow>
+          </>
+        )}
+
+        <InfoRow label="Possible Times">
           {eventRange.timeRange.from?.toLocaleTimeString([], {
             hour: "2-digit",
             minute: "2-digit",
@@ -67,20 +76,28 @@ export function EventInfo({ eventRange }: { eventRange: EventRange }) {
             hour: "2-digit",
             minute: "2-digit",
           })}
-        </div>
-      </div>
-      <div>
-        <span className="font-medium text-blue dark:text-red">
-          Original Event Timezone:
-        </span>
-        <div>{eventRange.timezone}</div>
-      </div>
-      <div>
-        <span className="font-medium text-blue dark:text-red">
-          Intended Duration:
-        </span>
-        <div>{eventRange.duration} minutes</div>
+        </InfoRow>
+
+        <InfoRow label="Timezone">{eventRange.timezone}</InfoRow>
+        <InfoRow label="Intended Duration">
+          {eventRange.duration} minutes
+        </InfoRow>
       </div>
     </section>
+  );
+}
+
+function InfoRow({
+  label,
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <div className="font-medium text-blue dark:text-red">{label}</div>
+      <div className="">{children}</div>
+    </div>
   );
 }
