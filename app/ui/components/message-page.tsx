9f354d5ebd013d3149e7e95b393c7488c21aff57
jzgom067 <jzgombic3@gmail.com>
2025-10-02 17:39:50 -0400

    Change secondary button to use border

--- app/ui/components/message-page.tsx
+++ app/ui/components/message-page.tsx
@@ -29,7 +29,7 @@ export default function MessagePage({
             className={`mb-2 cursor-pointer rounded-full px-4 py-2 font-medium transition ${
               button.type === "primary"
                 ? "bg-blue dark:bg-red"
-                : "outline-2 outline-blue hover:bg-blue-100 dark:outline-red dark:hover:bg-red/25"
+                : "border border-2 border-blue hover:bg-blue-100 dark:border-red dark:hover:bg-red/25"
             }`}
           >
             {button.label}
