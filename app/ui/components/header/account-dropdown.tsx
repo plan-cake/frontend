b0644e61c1391e1f4bf374da574ae96c5d41e1d9
jzgom067 <jzgombic3@gmail.com>
2025-10-08 01:54:37 -0400

    Add icons to dropdown items

--- app/ui/components/header/account-dropdown.tsx
+++ app/ui/components/header/account-dropdown.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+import { cn } from "@/app/_lib/classname";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { DashboardIcon, ExitIcon } from "@radix-ui/react-icons";
 
 import { forwardRef, ReactNode } from "react";
 
@@ -13,8 +14,14 @@ const AccountDropdown = ({ children }: { children: ReactNode }) => {
           className="frosted-glass z-50 mr-4 rounded-md p-1"
           sideOffset={16}
         >
-          <DropdownItem>Dashboard</DropdownItem>
-          <DropdownItem>Log Out</DropdownItem>
+          <DropdownItem>
+            <DashboardIcon className="h-4 w-4" />
+            Dashboard
+          </DropdownItem>
+          <DropdownItem>
+            <ExitIcon className="h-4 w-4" />
+            Log Out
+          </DropdownItem>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
@@ -30,7 +37,10 @@ const DropdownItem = forwardRef<HTMLDivElement, DropdownItemProps>(
   ({ onSelect, children }, ref) => {
     return (
       <DropdownMenu.Item
-        className="text-md cursor-pointer rounded-sm p-2 text-right leading-none hover:outline-none data-[highlighted]:bg-red-200 dark:data-[highlighted]:bg-violet-400"
+        className={cn(
+          "flex flex-row items-center justify-between gap-4",
+          "text-md cursor-pointer rounded-sm p-2 leading-none hover:outline-none data-[highlighted]:bg-red-200 dark:data-[highlighted]:bg-violet-400",
+        )}
         ref={ref}
         onSelect={onSelect}
       >
