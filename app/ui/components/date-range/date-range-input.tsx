1d0edca81daae357ec2e786a8e4974ac4e57ef6c
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-07 21:24:57 -0400

    update date range input to read only

--- app/ui/components/date-range/date-range-input.tsx
+++ app/ui/components/date-range/date-range-input.tsx
@@ -3,13 +3,11 @@ import { format } from "date-fns";
 type DateRangeInputProps = {
   startDate: Date;
   endDate: Date;
-  onChangeSpecific?: (key: "from" | "to", value: Date) => void;
 };
 
 export default function DateRangeInput({
   startDate,
   endDate,
-  onChangeSpecific,
 }: DateRangeInputProps) {
   const displayFrom = startDate ? format(startDate, "EEE MMMM d, yyyy") : "";
   const displayTo = endDate ? format(endDate, "EEE MMMM d, yyyy") : "";
@@ -18,11 +16,10 @@ export default function DateRangeInput({
       <div className="flex space-x-4">
         <label className="text-gray-400 md:hidden">FROM</label>
         <input
-          // size={10}
           value={displayFrom}
-          onChange={(e) => onChangeSpecific?.("from", new Date(e.target.value))}
           className="text-blue focus:outline-none dark:text-red"
           aria-label="Start date"
+          readOnly
         />
       </div>
 
@@ -31,11 +28,10 @@ export default function DateRangeInput({
       <div className="flex space-x-4">
         <label className="text-gray-400 md:hidden">UNTIL</label>
         <input
-          // size={10}
           value={displayTo}
-          onChange={(e) => onChangeSpecific?.("to", new Date(e.target.value))}
           className="text-blue focus:outline-none md:text-end dark:text-red"
           aria-label="End date"
+          readOnly
         />
       </div>
     </form>
