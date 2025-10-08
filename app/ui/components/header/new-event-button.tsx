fd2ef0b0c5ba0bc411a385466f74c57cda5d6b4b
jzgom067 <jzgombic3@gmail.com>
2025-10-08 16:39:26 -0400

    Add plus icon to new event button

--- app/ui/components/header/new-event-button.tsx
+++ app/ui/components/header/new-event-button.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import Link from "next/link";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 export default function NewEventButton() {
   return (
     <Link
-      className="cursor-pointer rounded-full bg-red px-4 py-2 font-medium"
+      className="flex cursor-pointer flex-row items-center gap-1 rounded-full bg-red py-2 pr-4 pl-2.5 font-medium"
       href="/new-event"
       aria-label="Create new event"
     >
+      <PlusIcon className="h-5 w-5" />
       New Event
     </Link>
   );
