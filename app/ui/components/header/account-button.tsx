0271f39b407435bd91635ff8d5ed5237f39cd6d6
jzgom067 <jzgombic3@gmail.com>
2025-10-08 16:52:59 -0400

    Change account button style to frosted glass
    It looked weird to have both the new event and log in buttons be red around the frosted glass theme toggle.
    
    Also, the log in button has an annoying 1.5 padding instead of 2, just to keep the size consistent with the theme toggle and account buttons. The joys of using outlines.

--- app/ui/components/header/account-button.tsx
+++ app/ui/components/header/account-button.tsx
@@ -45,7 +45,7 @@ export default function AccountButton() {
   } else {
     return (
       <Link
-        className="cursor-pointer rounded-full bg-red p-2 px-4 py-2 font-medium"
+        className="frosted-glass cursor-pointer rounded-full px-4 py-1.5 font-medium"
         href="/login"
         aria-label="Log in"
       >
