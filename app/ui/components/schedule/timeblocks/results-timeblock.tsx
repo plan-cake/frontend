2db3c7de6b2a2d0461eb5a963f543ac56af65784
jzgom067 <jzgombic3@gmail.com>
2025-10-15 00:44:45 -0400

    Fix prop error for results timeblock

--- app/ui/components/schedule/timeblocks/results-timeblock.tsx
+++ app/ui/components/schedule/timeblocks/results-timeblock.tsx
@@ -96,7 +96,7 @@ export default function ResultsTimeBlock({
             isHovered={isHovered}
             gridColumn={gridColumn}
             gridRow={gridRow}
-            onMouseEnter={() => {
+            onPointerEnter={() => {
               onHoverSlot?.(localSlotIso);
             }}
           />
