0ccd9e8daf140e8a504811e23ec27e8d377e8eb4
jzgom067 <jzgombic3@gmail.com>
2025-10-16 11:26:48 -0400

    Fix edit event date inconsistency

--- app/_utils/submit-event.tsx
+++ app/_utils/submit-event.tsx
@@ -14,9 +14,9 @@ export type EventSubmitData = {
 };
 
 const formatDate = (date: Date): string => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
-  const day = String(date.getDate()).padStart(2, "0");
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day = String(date.getUTCDate()).padStart(2, "0");
   return `${year}-${month}-${day}`;
 };
 
