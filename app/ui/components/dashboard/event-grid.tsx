aa653961ada7f7926268322e1b5650e9f57312f2
jzgom067 <jzgombic3@gmail.com>
2025-10-18 02:48:42 -0400

    Fix key error

--- app/ui/components/dashboard/event-grid.tsx
+++ app/ui/components/dashboard/event-grid.tsx
@@ -5,8 +5,8 @@ export type EventGridProps = DashboardEventProps[];
 export default function EventGrid({ events }: { events: EventGridProps }) {
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {events.map((data: DashboardEventProps) => (
-        <DashboardEvent key={data.code} {...data} />
+      {events.map((data: DashboardEventProps, index: number) => (
+        <DashboardEvent key={index} {...data} />
       ))}
     </div>
   );
