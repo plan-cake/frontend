e45434c1f261eaf0bf903dfa7372e86174a48dd2
jzgom067 <jzgombic3@gmail.com>
2025-10-18 13:32:46 -0400

    Add hover states to dashboard elements

--- app/ui/components/dashboard/dashboard-event.tsx
+++ app/ui/components/dashboard/dashboard-event.tsx
@@ -2,6 +2,7 @@ import { ClockIcon, CopyIcon, Pencil1Icon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import DateRangeRow from "./date-range-row";
 import WeekdayRow from "./weekday-row";
+import { cn } from "@/app/_lib/classname";
 
 export type DashboardEventProps = {
   myEvent: boolean;
@@ -39,7 +40,7 @@ export default function DashboardEvent({
 
   return (
     <Link href={`/${code}`}>
-      <div className="flex w-full flex-col rounded-lg bg-white p-4 dark:bg-violet">
+      <div className="flex w-full flex-col rounded-lg bg-white p-4 transition-shadow hover:shadow-lg hover:shadow-black/25 dark:bg-violet">
         <div className="rounded bg-white text-lg font-bold dark:bg-violet">
           {title}
         </div>
@@ -64,14 +65,22 @@ export default function DashboardEvent({
         <div className="mt-2 flex items-center gap-2">
           <button
             onClick={copyLink}
-            className="flex cursor-pointer items-center gap-0.5 rounded-full border border-violet px-2 py-1.5 dark:border-white"
+            className={cn(
+              "flex cursor-pointer items-center gap-0.5 rounded-full border border-violet px-2 py-1.5 dark:border-white",
+              "transition hover:bg-violet/25 dark:hover:bg-white/25",
+            )}
           >
             <CopyIcon className="h-4 w-4" />
             <span className="ml-1 text-xs">Copy Link</span>
           </button>
           {myEvent && (
             <Link href={`/${code}/edit`}>
-              <div className="w-fit rounded-full border border-violet p-1.5 dark:border-white">
+              <div
+                className={cn(
+                  "w-fit rounded-full border border-violet p-1.5 dark:border-white",
+                  "transition hover:bg-violet/25 dark:hover:bg-white/25",
+                )}
+              >
                 <Pencil1Icon className="h-4 w-4" />
               </div>
             </Link>
