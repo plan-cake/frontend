f9a32b966034cc54cfc277bfaec5b59e6022c45c
jzgom067 <jzgombic3@gmail.com>
2025-10-19 14:30:48 -0400

    Re-add header padding

--- app/ui/components/header/header.tsx
+++ app/ui/components/header/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   }
 
   return (
-    <header className="fixed top-0 z-40 h-25 w-full pt-4">
+    <header className="fixed top-0 z-40 h-25 w-full px-4 pt-4">
       <nav className="flex w-full max-w-[1440px] justify-between">
         <LogoArea />
 
