0d648a474182c4af0e939bef505dd9dc2836af0a
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-19 01:36:54 -0400

    fixed dashboard spacing

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -2,7 +2,7 @@ import HeaderSpacer from "@/app/ui/components/header/header-spacer";
 
 export default function Loading() {
   return (
-    <div className="flex min-h-screen flex-col gap-4 p-6">
+    <div className="flex min-h-screen flex-col gap-4 pr-6 pl-6">
       <HeaderSpacer />
 
       <h1 className="text-2xl font-bold">Dashboard</h1>
