27ccdb1056389d7d6c55609c6916103603f82147
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-27 12:30:53 -0400

    add linting rules for imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import prettierPlugin from "eslint-plugin-prettier";
+import pluginImport from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,11 +15,46 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
   {
+    files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
       prettier: prettierPlugin,
+      import: pluginImport,
     },
+
     rules: {
       "prettier/prettier": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          pathGroups: [
+            { pattern: "react", group: "external", position: "before" },
+            { pattern: "@/**", group: "internal" },
+          ],
+          pathGroupsExcludedImportTypes: ["react"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              // This glob pattern matches "../", "../../", etc.
+              group: ["..*"],
+              message:
+                "Use absolute imports with @/ prefix instead of relative parent imports.",
+            },
+          ],
+        },
+      ],
     },
   },
 ];
