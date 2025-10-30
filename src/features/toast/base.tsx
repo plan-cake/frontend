c6e64db9fc915203305d29b54c8c662b133e0059
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-29 22:06:18 -0400

    add delete toast badge

--- src/features/toast/base.tsx
+++ src/features/toast/base.tsx
@@ -1,3 +1,4 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
 import * as Toast from "@radix-ui/react-toast";
 
 import { cn } from "@/lib/utils/classname";
@@ -20,7 +21,7 @@ export default function BaseToast({
   return (
     <Toast.Root
       className={cn(
-        "grid grid-cols-[auto_auto] items-center gap-x-[15px] rounded-full px-6 py-3 text-white shadow-xl",
+        "group grid grid-cols-[auto_auto] items-center gap-x-[15px] rounded-full px-6 py-3 text-white shadow-xl",
         toastStyle,
         "data-[state=closed]:animate-slideOut data-[state=open]:animate-slideIn data-[swipe=end]:animate-swipeOut data-[swipe=cancel]:translate-x-0 data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:transition-[transform_200ms_ease-out]",
       )}
@@ -30,14 +31,27 @@ export default function BaseToast({
     >
       {icon}
 
-      <Toast.Title className="col-start-2 flex text-sm font-bold">
-        {title}
-      </Toast.Title>
-      <Toast.Description asChild>
-        <div className="col-start-2 m-0 text-[13px] leading-[1.3]">
-          {message}
-        </div>
-      </Toast.Description>
+      <div className="col-start-2 flex flex-col">
+        <Toast.Title className="flex text-sm font-bold">{title}</Toast.Title>
+        <Toast.Description asChild>
+          <div className="m-0 text-[13px] leading-[1.3]">{message}</div>
+        </Toast.Description>
+      </div>
+
+      <Toast.Close asChild>
+        <button
+          type="button"
+          aria-label="Close"
+          className={cn(
+            "col-start-3 row-span-2 flex h-6 w-6 items-center justify-center rounded-full",
+            "opacity-0 transition-all",
+            "focus:opacity-100 group-hover:opacity-100",
+            "hover:bg-black/20 focus:outline-none focus:ring-2 focus:ring-white/50",
+          )}
+        >
+          <Cross2Icon className="h-4 w-4" />
+        </button>
+      </Toast.Close>
     </Toast.Root>
   );
 }
