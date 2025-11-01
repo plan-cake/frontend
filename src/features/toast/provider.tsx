08ffb92995451ab6e9a9a37f9a38fff78516a721
jzgom067 <jzgombic3@gmail.com>
2025-11-01 19:39:46 -0400

    Remove toast borders
    Co-Authored-By: Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>

--- src/features/toast/provider.tsx
+++ src/features/toast/provider.tsx
@@ -32,13 +32,13 @@ function getToastIcon(iconType: ToastType) {
 function getToastStyle(type: ToastType) {
   switch (type) {
     case "error":
-      return "border-red bg-red border dark:border-red-400";
+      return "bg-red";
     case "copy":
-      return "border-lion bg-lion border";
+      return "bg-lion";
     case "success":
-      return "border-lion bg-lion border";
+      return "bg-lion";
     default:
-      return "border-blue bg-blue border";
+      return "bg-blue";
   }
 }
 
