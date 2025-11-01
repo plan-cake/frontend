8eed6c77fae6ad6e67cb5810217c2b33353aa91a
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-11-01 09:53:53 -0400

    update handle error file

--- src/lib/utils/api/handle-api-error.ts
+++ src/lib/utils/api/handle-api-error.ts
@@ -1,5 +1,11 @@
 import { notFound } from "next/dist/client/components/navigation";
 
+type ApiErrorResponse = {
+  error: {
+    [key: string]: string[];
+  };
+};
+
 function snakeToTitleCase(str: string): string {
   return str
     .split("_")
@@ -7,13 +13,59 @@ function snakeToTitleCase(str: string): string {
     .join(" ");
 }
 
-type ApiErrorResponse = {
-  error: {
-    [key: string]: string[];
-  };
-};
+export default function handleErrorResponse(
+  status: number,
+  errors: ApiErrorResponse,
+): never {
+  // handle 404 separately to trigger Next.js notFound page
+  if (status === 404) {
+    notFound();
+  }
 
-export default function formatApiError(errors: ApiErrorResponse): string {
+  // stringify the server message for error
+  const serverMessage = JSON.stringify(errors);
+  let errorMessage = formatApiError(errors);
+  if (process.env.NODE_ENV === "development") {
+    console.log("Formatted API Error:", errorMessage);
+  }
+
+  let title = "Oops! Something went wrong.";
+
+  switch (status) {
+    case 400:
+      errorMessage =
+        "It looks like your request wasn't quite right. Please double-check the information you sent and give it another try.";
+      break;
+    case 403:
+      title = "Access Restricted";
+      errorMessage =
+        "Sorry, it seems you don't have permission to view this. If you think you should have access, you might want to check that you're logged into the correct account.";
+      break;
+    case 429:
+      title = "Woah, slow down!";
+      errorMessage = errorMessage
+        ? "You're making requests too quickly. " + errorMessage
+        : "You've made too many requests in a short period. Please wait a moment before trying again.";
+      break;
+    case 500:
+      title = "Man :(";
+      errorMessage = "Something went wrong on our end. Please try again later.";
+      break;
+  }
+
+  const errorDetails = {
+    status: status,
+    title: title,
+    message: errorMessage,
+  };
+
+  // throw an error with a JSON string message to preserve structure
+  throw new Error(JSON.stringify(errorDetails), {
+    cause: serverMessage,
+  });
+}
+
+export function formatApiError(errors: ApiErrorResponse): string {
   let errorMessage = "";
   let generalMessage = "";
   const errorFields = errors.error;
@@ -41,53 +93,4 @@ export default function formatApiError(errors: ApiErrorResponse): string {
   }
 
   return "An unknown error has occurred.";
-}
-
-export function handleErrorResponse(
-  status: number,
-  errors: ApiErrorResponse,
-): never {
-  // handle 404 separately to trigger Next.js notFound page
-  if (status === 404) {
-    notFound();
-  }
-
-  // stringify the server message for error
-  const serverMessage = JSON.stringify(errors);
-  let errorMessage = formatApiError(errors);
-  console.log("Formatted API Error:", errorMessage);
-
-  let title = "Oops! Something went wrong.";
-
-  switch (status) {
-    case 400:
-      title = title;
-      errorMessage =
-        "It looks like your request wasn't quite right. Please double-check the information you sent and give it another try.";
-      break;
-    case 403:
-      title = "Access Restricted";
-      errorMessage =
-        "Sorry, it seems you don't have permission to view this. If you think you should have access, you might want to check that you're logged into the correct account.";
-      break;
-    case 429:
-      title = "Woah, slow down!";
-      errorMessage = "You're making requests too quickly. " + errorMessage;
-      break;
-    case 500:
-      title = "Man :(";
-      errorMessage = "Something went wrong on our end. Please try again later.";
-      break;
-  }
-
-  const errorDetails = {
-    status: status,
-    title: title,
-    message: errorMessage,
-  };
-
-  // throw an error with a JSON string message to preserve structure
-  throw new Error(JSON.stringify(errorDetails), {
-    cause: serverMessage,
-  });
 }
