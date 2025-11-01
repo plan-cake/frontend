2b2c57ba7c0794513920b70c47e1394aa98af7a9
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-31 21:54:12 -0400

    format not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,15 @@
 export default function NotFound() {
-  return <div>404 - Page Not Found :p</div>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center rounded-lg p-8 text-center">
+      <h1 className="font-display text-lion mb-4 block text-5xl leading-none md:text-[13rem]">
+        404
+      </h1>
+      <h2 className="mb-4 text-2xl font-bold">Not found :(</h2>
+
+      <p className="mb-6 max-w-md">
+        It looks you might have clicked on a bad link. Please check the URL and
+        try again!
+      </p>
+    </div>
+  );
 }
