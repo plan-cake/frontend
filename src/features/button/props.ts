9ad879d838304f55257570bfd8fb603a5488da95
jzgom067 <jzgombic3@gmail.com>
2025-11-02 02:31:46 -0500

    Remove redundant prop documentation

--- src/features/button/props.ts
+++ src/features/button/props.ts
@@ -81,74 +81,28 @@ export type BaseButtonProps = {
 };
 
 type CommonButtonProps = {
-  /**
-     * The style of the button. There are four styles:
-     * - `primary`: An important button, filled with the main accent color.
-     * - `secondary`: A less important button, outlined with the main accent color.
-     * - `frosted glass`: A button with a frosted glass appearance, used only in the header.
-     * - `transparent`: A button with no background until hovered.
-     *
-     * `transparent` buttons cannot have icons.
-     */
+  /** @inheritdoc BaseButtonProps */
   buttonStyle: ButtonStyle;
-  /**
-   * The icon to display in the button.
-   *
-   * Should not include a className prop for styling, since it will be overridden.
-   */
+  /** @inheritdoc BaseButtonProps */
   icon?: ReactNode;
-  /**
-   * The text label of the button.
-   *
-   * If `shrinkOnMobile` is `true`, the label will be hidden on small screens.
-   */
+  /** @inheritdoc BaseButtonProps */
   label?: string;
-  /**
-   * If `true`, the button will hide its label on small screens, showing only the icon.
-   *
-   * This prop requires both an icon and a label to be provided.
-   * @default false
-   */
+  /** @inheritdoc BaseButtonProps */
   shrinkOnMobile?: boolean;
-  /**
-   * If `true`, the button will show a loading spinner and be unclickable.
-   * 
-   * Typically, the loading state is managed internally by the button when `onClick` is
-   * provided. However, this can be helpful if a button shouldn't be accessible until
-   * something else has loaded.
-   * @default false
-   */
+  /** @inheritdoc BaseButtonProps */
   loading?: boolean;
-  /**
-   * Whether the button is disabled. A disabled button cannot be clicked and will have a
-   * distinct style.
-   * @default false
-   */
+  /** @inheritdoc BaseButtonProps */
   disabled?: boolean;
 };
 
 export type ActionButtonProps = CommonButtonProps & {
-  /**
-   * The function to call when the button is clicked.
-   *
-   * The function must return a boolean or a Promise that resolves to a boolean,
-   * indicating whether the action was successful.
-   */
+  /** @inheritdoc BaseButtonProps */
   onClick: () => Promise<boolean> | boolean;
-  /**
-   * If specified, the button will stay in a loading state after a successful action. This
-   * behavior should be used for buttons that trigger navigation, to avoid multiple clicks
-   * before the new page loads.
-   * 
-   * Otherwise, the button will return to a normal state no matter the result of
-   * `onClick`.
-   */
+  /** @inheritdoc BaseButtonProps */
   loadOnSuccess?: boolean;
 };
 
 export type LinkButtonProps = CommonButtonProps & {
-  /**
-   * The URL to navigate to when the button is clicked.
-   */
+  /** @inheritdoc BaseButtonProps */
   href: string;
 };
