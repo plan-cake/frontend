714bf8cc96060c33f1e60e49c59899153e6519e0
jzgom067 <jzgombic3@gmail.com>
2026-01-08 21:18:48 -0500

    Separate major and minor version data types

--- src/features/version-history/type.ts
+++ src/features/version-history/type.ts
@@ -1,16 +1,18 @@
 type MajorVersionNumber = `v${number}.${number}`;
 type MinorVersionNumber = `${MajorVersionNumber}.${number}`;
 
-type VersionData = {
+export type MinorVersionData = {
+    version: MinorVersionNumber;
+    date: Date;
+    changes: string[];
+};
+
+export type MajorVersionData = {
     version: MajorVersionNumber;
     date: Date;
     changes: string[];
     bugFixes?: string[];
-    minorVersions?: {
-        version: MinorVersionNumber;
-        date: Date;
-        changes: string[];
-    }[];
+    minorVersions?: MinorVersionData[];
 };
 
-export type VersionHistoryData = VersionData[];
+export type VersionHistoryData = MajorVersionData[];
