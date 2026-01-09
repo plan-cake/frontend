ab5062c94c0afc256563932ddf8460edefc932e2
Daniel Shi <144500568+danielshid@users.noreply.github.com>
2026-01-09 17:45:04 +0000

    fixed spacing between event-details and timezone picker panels for painting

--- src/app/(event)/[event-code]/painting/page-client.tsx
+++ src/app/(event)/[event-code]/painting/page-client.tsx
@@ -133,7 +133,7 @@ export default function ClientPage({
       {/* Main Content */}
       <div className="mb-12 flex h-fit flex-col gap-4 md:mb-0 md:flex-row">
         {/* Left Panel */}
-        <div className="md:top-25 h-fit w-full shrink-0 space-y-6 overflow-y-auto md:sticky md:w-80">
+        <div className="md:top-25 h-fit w-full shrink-0 space-y-4 overflow-y-auto md:sticky md:w-80">
           <div className="w-fit">
             <p
               className={`text-error text-right text-xs ${errors.displayName ? "visible" : "invisible"}`}
