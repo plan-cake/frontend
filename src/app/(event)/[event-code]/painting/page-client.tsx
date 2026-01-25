14d621eec8be5305e146b74ff33c3940e1b0362f
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-01-25 15:57:20 -0500

    update toast wording
    Co-Authored-By: Jack Zgombic <69125339+jzgom067@users.noreply.github.com>

--- src/app/(event)/[event-code]/painting/page-client.tsx
+++ src/app/(event)/[event-code]/painting/page-client.tsx
@@ -50,7 +50,7 @@ export default function ClientPage({
 
     const toastId = addToast(
       "info",
-      "Hold down shift to select multiple slots at once.",
+      "Click once and hold shift to select multiple slots without dragging. Works well if you'r on a trackpad!",
       {
         title: "SHIFT TIP",
         isPersistent: true,
