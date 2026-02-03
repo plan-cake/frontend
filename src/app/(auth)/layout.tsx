b3569d0fed5262e9bf59a7775c185ce9c9dd4b81
jzgom067 <jzgombic3@gmail.com>
2026-02-03 18:06:28 -0500

    Fix toast triggering on logic

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { useRouter } from "next/navigation";
 
@@ -17,11 +17,19 @@ export default function AuthLayout({
   const { loginState } = useAccount();
   const router = useRouter();
   const { addToast } = useToast();
+  const hasBeenLoggedOutRef = useRef(false);
 
   useEffect(() => {
+    if (loginState === "logged_out") {
+      hasBeenLoggedOutRef.current = true;
+    }
+
     if (loginState === "logged_in") {
-      router.replace("/dashboard");
-      addToast("info", MESSAGES.INFO_ALREADY_LOGGED_IN);
+      // Check if the user was logged out to avoid this triggering on login
+      if (!hasBeenLoggedOutRef.current) {
+        router.replace("/dashboard");
+        addToast("info", MESSAGES.INFO_ALREADY_LOGGED_IN);
+      }
     }
   }, [loginState, router, addToast]);
 
