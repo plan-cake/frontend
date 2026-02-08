5a742f99d4acf5c21ce38274db83d3f0a220faba
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-02-08 15:30:39 -0500

    show password criteria

--- src/components/text-input-field.tsx
+++ src/components/text-input-field.tsx
@@ -6,6 +6,7 @@ import {
   ExclamationTriangleIcon,
 } from "@radix-ui/react-icons";
 
+import PasswordCriteria from "@/features/auth/components/password-criteria";
 import { cn } from "@/lib/utils/classname";
 
 type FieldType = "text" | "email" | "password";
@@ -21,6 +22,8 @@ type TextInputFieldProps = {
   outlined?: boolean;
   error?: string;
   classname?: string;
+  showPasswordCriteria?: boolean;
+  passwordCriteria?: { [key: string]: boolean };
 };
 
 export default function TextInputField(props: TextInputFieldProps) {
@@ -35,6 +38,8 @@ export default function TextInputField(props: TextInputFieldProps) {
     error,
     outlined,
     classname,
+    showPasswordCriteria = false,
+    passwordCriteria = {},
   } = props;
   const [showPassword, setShowPassword] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
@@ -45,113 +50,124 @@ export default function TextInputField(props: TextInputFieldProps) {
   const inputType = isPassword ? (showPassword ? "text" : "password") : type;
 
   return (
-    <div className={cn("relative mb-4 w-full", classname)}>
-      {/* --- input field --- */}
-      <input
-        id={id}
-        type={inputType}
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        onFocus={() => {
-          setIsFocused(true);
-          onFocus?.();
-        }}
-        onBlur={() => {
-          setIsFocused(false);
-          onBlur?.();
-        }}
-        placeholder=" " // triggers placeholder-shown state for floating label
-        className={cn(
-          "text-foreground peer w-full bg-transparent py-2 focus:outline-none",
-          outlined ? "rounded-full px-5" : "border-b-1 px-2",
-          isPassword && "pr-10",
-
-          // borders and colors
-          error
-            ? "border-error text-error" // error
-            : "border-foreground", // default
-        )}
-      />
-      {/* --- floating label --- */}
-      <label
-        htmlFor={id}
-        className={cn(
-          "absolute origin-[0_0] cursor-text px-2",
-          "transition-[top,scale] duration-300 ease-in-out",
-          outlined ? "left-4" : "left-1",
-          classname,
-
-          // --- Floating Animation ---
-          // State when placeholder is shown (input is empty)
-          "peer-placeholder-shown:top-2 peer-placeholder-shown:scale-100",
-
-          // State when floated (on focus or when value exists)
-          "peer-focus:top-[-0.55rem] peer-focus:scale-75",
-          "peer-[:not(:placeholder-shown)]:top-[-0.55rem] peer-[:not(:placeholder-shown)]:scale-75",
-          "peer-[:autofill]:top-[-0.55rem] peer-[:autofill]:scale-75",
-
-          outlined
-            ? ""
-            : "peer-focus:top-[-1rem] peer-[:not(:placeholder-shown)]:top-[-1rem]",
-
-          // colors
-          error
-            ? "text-error" // error
-            : "text-foreground/50", // default
-        )}
-      >
-        {error ? (
-          <span className="flex items-center gap-1">
-            <ExclamationTriangleIcon
-              className={`${classname}`}
-              aria-hidden="true"
-            />
-            {error}
-          </span>
-        ) : (
-          label
-        )}
-      </label>
-
-      {outlined && (
-        <fieldset
-          aria-hidden="true"
+    <div className={`w-full ${classname || ""}`}>
+      <div className={cn("relative mb-4 w-full", classname)}>
+        {/* --- input field --- */}
+        <input
+          id={id}
+          type={inputType}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          onFocus={() => {
+            setIsFocused(true);
+            onFocus?.();
+          }}
+          onBlur={() => {
+            setIsFocused(false);
+            onBlur?.();
+          }}
+          placeholder=" " // triggers placeholder-shown state for floating label
           className={cn(
-            "pointer-events-none absolute inset-0 z-10 h-full rounded-full border bg-transparent transition-[border]",
-            "peer-[:autofill]:[&>legend]:w-auto peer-[:autofill]:[&>legend]:max-w-full",
-            // Border Colors
-            error ? "border-error" : "border-foreground",
-            isFocused &&
-              (error ? "border-error border-2" : "border-foreground border-2"),
+            "text-foreground peer w-full bg-transparent py-2 focus:outline-none",
+            outlined ? "rounded-full px-5" : "border-b-1 px-2",
+            isPassword && "pr-10",
+
+            // borders and colors
+            error
+              ? "border-error text-error" // error
+              : "border-foreground", // default
+          )}
+        />
+        {/* --- floating label --- */}
+        <label
+          htmlFor={id}
+          className={cn(
+            "absolute origin-[0_0] cursor-text px-2",
+            "transition-[top,scale] duration-300 ease-in-out",
+            outlined ? "left-4" : "left-1",
+            classname,
+
+            // --- Floating Animation ---
+            // State when placeholder is shown (input is empty)
+            "peer-placeholder-shown:top-2 peer-placeholder-shown:scale-100",
+
+            // State when floated (on focus or when value exists)
+            "peer-focus:top-[-0.55rem] peer-focus:scale-75",
+            "peer-[:not(:placeholder-shown)]:top-[-0.55rem] peer-[:not(:placeholder-shown)]:scale-75",
+            "peer-[:autofill]:top-[-0.55rem] peer-[:autofill]:scale-75",
+
+            outlined
+              ? ""
+              : "peer-focus:top-[-1rem] peer-[:not(:placeholder-shown)]:top-[-1rem]",
+
+            // colors
+            error
+              ? "text-error" // error
+              : "text-foreground/50", // default
           )}
         >
-          <legend
+          {error ? (
+            <span className="flex items-center gap-1">
+              <ExclamationTriangleIcon
+                className={`${classname}`}
+                aria-hidden="true"
+              />
+              {error}
+            </span>
+          ) : (
+            label
+          )}
+        </label>
+
+        {outlined && (
+          <fieldset
+            aria-hidden="true"
             className={cn(
-              "invisible ml-4 h-0 overflow-hidden transition-[width] duration-300",
-              isFloating ? "w-auto" : "w-0",
+              "pointer-events-none absolute inset-0 z-10 h-full rounded-full border bg-transparent transition-[border]",
+              "peer-[:autofill]:[&>legend]:w-auto peer-[:autofill]:[&>legend]:max-w-full",
+              // Border Colors
+              error ? "border-error" : "border-foreground",
+              isFocused &&
+                (error
+                  ? "border-error border-2"
+                  : "border-foreground border-2"),
             )}
           >
-            <span className="px-1 text-xs font-medium">
-              {error ? error : label}
-            </span>
-            {error && <span className="inline-block w-4" />}
-          </legend>
-        </fieldset>
-      )}
+            <legend
+              className={cn(
+                "invisible ml-4 h-0 overflow-hidden transition-[width] duration-300",
+                isFloating ? "w-auto" : "w-0",
+              )}
+            >
+              <span className="px-1 text-xs font-medium">
+                {error ? error : label}
+              </span>
+              {error && <span className="inline-block w-4" />}
+            </legend>
+          </fieldset>
+        )}
 
-      {/* --- trailing icon --- */}
-      {isPassword && (
-        <button
-          type="button"
-          onClick={() => setShowPassword(!showPassword)}
-          className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
-        >
-          {showPassword ? (
-            <EyeOpenIcon className="h-5 w-5" />
-          ) : (
-            <EyeNoneIcon className="h-5 w-5" />
-          )}
-        </button>
+        {/* --- trailing icon --- */}
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+          >
+            {showPassword ? (
+              <EyeOpenIcon className="h-5 w-5" />
+            ) : (
+              <EyeNoneIcon className="h-5 w-5" />
+            )}
+          </button>
+        )}
+      </div>
+
+      {/* Password Criteria */}
+      {showPasswordCriteria && (
+        <div className="-mt-2 mb-2 w-full px-4">
+          <PasswordCriteria criteria={passwordCriteria} />
+        </div>
       )}
     </div>
   );
