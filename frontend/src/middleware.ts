a55a79e62e602773f479223f8215894fd1cc8499
jzgom067 <jzgombic3@gmail.com>
2026-02-16 14:48:28 -0500

    Add DEBUG flag functionality to middleware
    This way the cookie deletion doesn't get in the way of local testing.

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,28 +4,30 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
-  const cookieNames = ["account_sess_token", "guest_sess_token"];
-  const hasLegacyCookies = cookieNames.some((name) =>
-    request.cookies.has(name)
-  );
+  if (process.env.DEBUG !== "true") {
+    const cookieNames = ["account_sess_token", "guest_sess_token"];
+    const hasLegacyCookies = cookieNames.some((name) =>
+      request.cookies.has(name)
+    );
 
-  // If the user has auth cookies...
-  if (hasLegacyCookies) {
+    // If the user has auth cookies...
+    if (hasLegacyCookies) {
 
-    // We defensively try to DELETE the Host-Only (legacy) cookies on every request.
-    // By setting Max-Age=0 and omitting the Domain attribute, we target the Host-Only version.
-    // The valid Domain cookie (.example.com) will remain untouched because the browser
-    // sees them as different scopes.
+      // We defensively try to DELETE the Host-Only (legacy) cookies on every request.
+      // By setting Max-Age=0 and omitting the Domain attribute, we target the Host-Only version.
+      // The valid Domain cookie (.example.com) will remain untouched because the browser
+      // sees them as different scopes.
 
-    // This logic can only be removed AFTER February 15, 2027 (one year from now) just
-    // to be safe, since long session cookies have a 1 year lifetime
+      // This logic can only be removed AFTER February 15, 2027 (one year from now) just
+      // to be safe, since long session cookies have a 1 year lifetime
 
-    cookieNames.forEach((name) => {
-      response.headers.append(
-        "Set-Cookie",
-        `${name}=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax`
-      );
-    });
+      cookieNames.forEach((name) => {
+        response.headers.append(
+          "Set-Cookie",
+          `${name}=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax`
+        );
+      });
+    }
   }
 
   return response;
