45d58048afa839bf6ed4d5d0c58fc56e75771482
jzgom067 <jzgombic3@gmail.com>
2026-02-17 18:13:01 -0500

    Move confirmation logic to dashboard page

--- frontend/src/app/dashboard/page-client.tsx
+++ frontend/src/app/dashboard/page-client.tsx
@@ -1,37 +1,73 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import Link from "next/link";
 
 import HeaderSpacer from "@/components/header-spacer";
 import SegmentedControl from "@/components/segmented-control";
 import { useAccount } from "@/features/account/context";
-import EventGrid, {
-  EventGridProps,
-} from "@/features/dashboard/components/event-grid";
-import { Banner } from "@/features/system-feedback";
+import { DashboardEventProps } from "@/features/dashboard/components/event";
+import EventGrid from "@/features/dashboard/components/event-grid";
+import { Banner, ConfirmationDialog } from "@/features/system-feedback";
 
 type DashboardTab = "created" | "participated";
 
 export type DashboardPageProps = {
-  created_events: EventGridProps;
-  participated_events: EventGridProps;
+  created_events: DashboardEventProps[];
+  participated_events: DashboardEventProps[];
 };
 
 export default function ClientPage({
   created_events,
   participated_events,
 }: DashboardPageProps) {
+  const [createdEvents, setCreatedEvents] = useState(created_events);
+  const [participatedEvents, setParticipatedEvents] =
+    useState(participated_events);
   const [tab, setTab] = useState<DashboardTab>(
     !created_events.length && participated_events.length
       ? "participated"
       : "created",
   );
+
+  const [confirmationOpen, setConfirmationOpen] = useState(false);
+  const eventToDelete = useRef<string | null>(null);
+
   const { loginState } = useAccount();
 
   const currentTabEvents =
-    tab === "created" ? created_events : participated_events;
+    tab === "created" ? createdEvents : participatedEvents;
+
+  const deleteEvent = (eventCode: string) => {
+    // Immediate UI update
+    if (tab === "created") {
+      setCreatedEvents(createdEvents.filter((e) => e.code !== eventCode));
+    } else {
+      setParticipatedEvents(
+        participatedEvents.filter((e) => e.code !== eventCode),
+      );
+    }
+
+    // API call
+    try {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/event/delete/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ event_code: eventCode }),
+      });
+    } catch (e) {
+      console.error("Error deleting event:", e);
+    }
+  };
+
+  const handleDeleteEvent = (eventCode: string) => {
+    eventToDelete.current = eventCode;
+    setConfirmationOpen(true);
+  };
 
   return (
     <div className="flex min-h-screen flex-col gap-4 px-6 pb-4">
@@ -64,7 +100,10 @@ export default function ClientPage({
         </div>
         <div className="p-4 pt-2">
           {currentTabEvents.length ? (
-            <EventGrid events={currentTabEvents} />
+            <EventGrid
+              events={currentTabEvents}
+              onDeleteEvent={handleDeleteEvent}
+            />
           ) : (
             <div className="flex flex-col items-center justify-center gap-4 p-4 text-center italic opacity-75">
               <div>
@@ -77,6 +116,18 @@ export default function ClientPage({
           )}
         </div>
       </div>
+      <ConfirmationDialog
+        type="delete"
+        autoClose={true}
+        title="Delete Event"
+        description="Are you sure you want to delete this event?"
+        open={confirmationOpen}
+        onOpenChange={setConfirmationOpen}
+        onConfirm={() => {
+          deleteEvent(eventToDelete.current!);
+          return true;
+        }}
+      />
     </div>
   );
 }
